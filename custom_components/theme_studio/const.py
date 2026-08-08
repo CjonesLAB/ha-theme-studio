a5636a4b21d8e77c883f30cc8f6e51a33fac8701
Carsten <81263879+CjonesLAB@users.noreply.github.com>
2026-08-09 01:50:00 +0200

    Release Theme Studio 0.4.4

--- custom_components/theme_studio/const.py
+++ custom_components/theme_studio/const.py
@@ -2,4 +2,8 @@
 
 DOMAIN = "theme_studio"
 NAME = "Theme Studio"
-VERSION = "0.1.0"
+VERSION = "0.4.4"
+
+GALLERY_BASE_URL = "https://ha-theme-studio.com"
+GALLERY_LIST_URL = f"{GALLERY_BASE_URL}/api/designs"
+GALLERY_DOWNLOAD_URL = f"{GALLERY_BASE_URL}/download"
